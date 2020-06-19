57e2ef8b04d22ede779c389e483f089008eeb356
unknown <exgin3538@yahoo.com>
2020-06-19 13:12:17 -0400

    tests for left btn

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -30,18 +30,33 @@ it('works when you click on the right arrow', function () {
   expect(queryByAltText('Photo by Pratik Patel on Unsplash')).toBeInTheDocument();
 });
 
-it('works when you click on the left arrow', function () {
-  const { queryByTestId, queryByAltText } = render(<Carousel />);
+// it('works when you click on the left arrow', function () {
+//   const { queryByTestId, queryByAltText } = render(<Carousel />);
 
-  // expect the second image to show | not the first
-  expect(queryByAltText('Photo by Pratik Patel on Unsplash')).toBeInTheDocument();
-  expect(queryByAltText('Photo by Richard Pasquarella on Unsplash')).not.toBeInTheDocument();
+//   // expect the second image to show | not the first
+//   expect(queryByAltText('Photo by Pratik Patel on Unsplash')).toBeInTheDocument();
+//   expect(queryByAltText('Photo by Richard Pasquarella on Unsplash')).not.toBeInTheDocument();
 
-  // move backwards in the carousel
-  const leftArrow = queryByTestId('left-arrow');
-  fireEvent.click(leftArrow);
+//   // move backwards in the carousel
+//   const leftArrow = queryByTestId('left-arrow');
+//   fireEvent.click(leftArrow);
 
-  // should move backwards to the last image, image 1
-  expect(queryByAltText('Photo by Richard Pasquarella on Unsplash')).toBeInTheDocument();
-  expect(queryByAltText('Photo by Pratik Patel on Unsplash')).not.toBeInTheDocument();
+//   // should move backwards to the last image, image 1
+//   expect(queryByAltText('Photo by Richard Pasquarella on Unsplash')).toBeInTheDocument();
+//   expect(queryByAltText('Photo by Pratik Patel on Unsplash')).not.toBeInTheDocument();
+// });
+
+it('should not show the left button', function () {
+  const { queryByTestId } = render(<Carousel />);
+
+  expect(queryByTestId('left-arrow')).not.toHaveClass();
+});
+
+it('should now show left button', function () {
+  const { queryByTestId } = render(<Carousel />);
+
+  const rightArrow = queryByTestId('right-arrow');
+  fireEvent.click(rightArrow);
+
+  expect(queryByTestId('left-arrow')).toHaveClass('fas fa-chevron-circle-left fa-2x');
 });
